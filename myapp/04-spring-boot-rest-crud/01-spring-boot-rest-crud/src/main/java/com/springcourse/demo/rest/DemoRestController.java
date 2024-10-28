316e7d737a865ea8d59d4de902e9992bae305fcb
toka98ahmed <toka98ahmed@gmail.com>
2024-10-28 15:08:05 +0300

    Edited DemoRestController.java

--- myapp/04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/springcourse/demo/rest/DemoRestController.java
+++ myapp/04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/springcourse/demo/rest/DemoRestController.java
@@ -1,4 +1,19 @@
 package com.springcourse.demo.rest;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
 public class DemoRestController {
+
+    // add code for endpoint
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+
 }
