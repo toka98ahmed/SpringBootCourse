8e45d4511510aa0a2be9c59c6ce72302f8d098e4
toka98ahmed <toka98ahmed@gmail.com>
2024-11-01 12:51:50 +0200

    Updated DemoSecurityConfig.java

--- myapp/05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/Security/DemoSecurityConfig.java
+++ myapp/05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/Security/DemoSecurityConfig.java
@@ -3,36 +3,73 @@ package com.luv2code.springboot.cruddemo.Security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
 
+
+    // add support for JDBC
+
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager(){
-        UserDetails John = User.builder()
-                .username("john")
-                .password("{noop}test123")  //no operation, stored as plain text
-                .roles("EMPLOYEE")
-                .build();
-
-
-        UserDetails Mary = User.builder()
-                .username("mary")
-                .password("{noop}test123")  //no operation, stored as plain text
-                .roles("EMPLOYEE", "MANAGER")
-                .build();
-
-
-        UserDetails Susan = User.builder()
-                .username("susan")
-                .password("{noop}test123")  //no operation, stored as plain text
-                .roles("EMPLOYEE", "MANAGER", "ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(John, Mary, Susan);
-
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
+        );
+
+        // use HTTP basic authentication
+
+        http.httpBasic(Customizer.withDefaults());
+
+        // disable CSRF
+        // Not required for REST APIs
+
+        http.csrf(csrf-> csrf.disable());
+
+        return http.build();
+    }
+
+    /**
+     @Bean
+     public InMemoryUserDetailsManager userDetailsManager(){
+     UserDetails John = User.builder()
+     .username("john")
+     .password("{noop}test123")  //no operation, stored as plain text
+     .roles("EMPLOYEE")
+     .build();
+
+
+     UserDetails Mary = User.builder()
+     .username("mary")
+     .password("{noop}test123")  //no operation, stored as plain text
+     .roles("EMPLOYEE", "MANAGER")
+     .build();
+
+
+     UserDetails Susan = User.builder()
+     .username("susan")
+     .password("{noop}test123")  //no operation, stored as plain text
+     .roles("EMPLOYEE", "MANAGER", "ADMIN")
+     .build();
+
+     return new InMemoryUserDetailsManager(John, Mary, Susan);
+
+     } **/
 }
